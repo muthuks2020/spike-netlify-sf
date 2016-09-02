3099aba44f10fa7cbe646ea0ad8abe6c4ce32b4f
Robert W Boyer <rwboyer@mac.com>
2016-09-02 14:37:57 -0400

    working but no file rename = PR needed for spike

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const htmlStandards = require('spike-html-standards')
 const cssStandards = require('spike-css-standards')
 const latest = require('babel-preset-latest')
+const sass = require('sass-loader')
+const extract_plug =  require('extract-text-webpack-plugin')
 
 module.exports = {
+  matchers: { css: '*.scss' },
+  module: {
+    loaders: [
+      { test: /\.scss$/, loaders: ['postcss-loader', 'sass-loader'] }
+    ]
+  },
   devtool: 'source-map',
-  ignore: ['**/layout.html', '**/_*', '**/.*'],
+  ignore: ['**/layout.html', '**/_*', '**/.*', 'bower_components/'],
   reshape: (ctx) => {
     return htmlStandards({
       parser: false,
